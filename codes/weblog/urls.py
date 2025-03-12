8d904cdee9ca4da137326bfcef3a1f1544d7a4e3
amir aspad <amiraspad1380@gmail.com>
2025-03-12 21:26:36 +0330

    add input and output css to style and create profile style in input.css

--- codes/weblog/urls.py
+++ codes/weblog/urls.py
@@ -3,8 +3,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
+from .views import a
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', a)
 ]
 
 if settings.DEBUG:
---
+++ codes/weblog/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def a(request):
+    return render(request, 'base.html ')
