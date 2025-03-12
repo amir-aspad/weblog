0bf76ef3b9eb2518f80834856fc563d77eab2635
amir aspad <amiraspad1380@gmail.com>
2025-03-12 21:06:20 +0330

    show profile in html templates

--- codes/blog/urls.py
+++ codes/blog/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'blog'
-urlpatterns = []
+urlpatterns = [
+    path('', views.AllPostView.as_view(), name='all')
+]
--- codes/blog/views.py
+++ codes/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+# import from blog app
+from .models import Blog
+
+class AllPostView(View):
+    template_name = 'blog/show_all.html'
+    
+    def get(self, request):
+        blogs = Blog.config.all()
+        
+        data = {
+            'blogs':blogs
+        }
+        return render(request, self.template_name, data)
