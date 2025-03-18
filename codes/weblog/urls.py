4e8ce4592d952e0714e5988aff5749979a20db5a
amir aspad <amiraspad1380@gmail.com>
2025-03-18 10:04:43 +0330

    messages add to template

--- codes/weblog/urls.py
+++ codes/weblog/urls.py
@@ -3,11 +3,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
-from .views import a
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', a)
 ]
 
 if settings.DEBUG:
--- codes/weblog/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def a(request):
-    return render(request, 'base.html ')
